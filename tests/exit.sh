22018e02a7a748ed3c6e55c8138cdbe490416127
Rebecca Hom <rhom001@hammer.cs.ucr.edu>
2015-11-06 21:13:39 -0800

    modified exit test shell script

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,65 +1,118 @@
-#!/bin/sh
-# exit.sh
-# tests bin/rshell with variations of exit command
+Script started on Fri 06 Nov 2015 09:09:56 PM PST
+]0;rhom001@hammer:~/rshell[?1034h[rhom001@hammer rshell]$ make
+mkdir bin
+g++ -Wall -Werror -ansi -pedantic -o bin/rshell src/main.cpp src/rshell.cpp
+]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ bin/rshell
+rhom001@hammer$ exit
+logout
+]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ bin/rshell
+rhom001@hammer$ e git status   && echo "Hello world" || exit
+# On branch master
+# Your branch is ahead of 'origin/master' by 3 commits.
+#   (use "git push" to publish your local commits)
+#
+# Changes not staged for commit:
+#   (use "git add <file>..." to update what will be committed)
+#   (use "git checkout -- <file>..." to discard changes in working directory)
+#
+#	[31mmodified:   README.md[m
+#	[31mmodified:   tests/exit.sh[m
+#
+# Untracked files:
+#   (use "git add <file>..." to include in what will be committed)
+#
+#	[31mbin/[m
+#	[31mrhom001[m
+no changes added to commit (use "git add" and/or "git commit -a")
+"Hello world"
+rhom001@hammer$ ex  ec  g cal && exit
+    November 2015   
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5 [7m 6[27m  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30
 
-# Makes the rshell executable
-make
-echo -e "Exit Command Test"
-
-# Test 1: Tests exit by itself
-echo -e "Test 1: Exit Command - Exit/Itself"
-bin/rshell << EOF
+logout
+]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ i[Kbin/rshell
+rhom001@hammer$ echo "Hello world" && exit  ; echo Goodbe ye || echo whata       "Whata    a bummer"
+"Hello world"
+logout
+]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ bin/rshell
+rhom001@hammer$ echo "Let's try a comment#ed exit" && exit
+"Let's try a comment#ed exit"
+logout
+]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ bin/rshell
+rhom001@hammer$ eco    ho   ho "Let's try this again" #&& exit; echo "Again?!?"
+"Let's try this again" 
+rhom001@hammer$ echo "That didn't work"                       echo "Hooray         Hooray && exit
+Hooray
+logout
+]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ bin/rshell
+rhom001@hammer$ #exit; echo "That exit was commented out"
+rhom001@hammer$ git status || exit; git log && exit; git sta         
+# On branch master
+# Your branch is ahead of 'origin/master' by 3 commits.
+#   (use "git push" to publish your local commits)
+#
+# Changes not staged for commit:
+#   (use "git add <file>..." to update what will be committed)
+#   (use "git checkout -- <file>..." to discard changes in working directory)
+#
+#	[31mmodified:   README.md[m
+#	[31mmodified:   tests/exit.sh[m
+#
+# Untracked files:
+#   (use "git add <file>..." to include in what will be committed)
+#
+#	[31mbin/[m
+#	[31mrhom001[m
+no changes added to commit (use "git add" and/or "git commit -a")
+[?1h=[33mcommit 3e1d070e508f45a8df4ac4c9284d069f07218eed[m[m
+Author: Rebecca Hom <rhom001@hammer.cs.ucr.edu>[m
+Date:   Fri Nov 6 21:02:51 2015 -0800[m
+[m
+    modified commented_command.sh[m
+[m
+[33mcommit 13ea3b4d3c6a51754045b57a6cb5cc9fb90531fe[m[m
+Author: Rebecca Hom <rhom001@hammer.cs.ucr.edu>[m
+Date:   Fri Nov 6 20:47:59 2015 -0800[m
+[m
+    updated multiple commands script test[m
+[m
+[33mcommit 2a160cb94cc4f7aebda71bfcd872f6552a34a9b5[m[m
+Author: Rebecca Hom <rhom001@hammer.cs.ucr.edu>[m
+Date:   Fri Nov 6 20:47:59 2015 -0800[m
+[m
+    updated single command script test[m
+[m
+[33mcommit dfdbf0b97dbfa1d599a9d8b1d9fb24aa90b57a06[m[m
+Author: Rebecca Hom <rhom001@hammer.cs.ucr.edu>[m
+Date:   Fri Nov 6 20:33:11 2015 -0800[m
+[m
+    modified username/hostname prompt to match example[m
+[m
+[33mcommit 2bfd6bb24917f5ed47286c121e68e7d608e6dcd5[m[m
+:[K[K[?1l>logout
+]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ git log && e[K[K[K[K[K[K[K[K[K status && exit
+# On branch master
+# Your branch is ahead of 'origin/master' by 3 commits.
+#   (use "git push" to publish your local commits)
+#
+# Changes not staged for commit:
+#   (use "git add <file>..." to update what will be committed)
+#   (use "git checkout -- <file>..." to discard changes in working directory)
+#
+#	[31mmodified:   README.md[m
+#	[31mmodified:   tests/exit.sh[m
+#
+# Untracked files:
+#   (use "git add <file>..." to include in what will be committed)
+#
+#	[31mbin/[m
+#	[31mrhom001[m
+no changes added to commit (use "git add" and/or "git commit -a")
 exit
-EOF
-echo -e "\n"
 
-# Test 2: Tests exit after single command
-echo -e "Test 2: Exit Command - Exit/Single Command"
-bin/rshell << EOF
-echo "Hello world"
-exit
-EOF
-echo -e "\n"
-
-# Test 3: Tests exit after multiple commands
-echo -e "Test 3: Exit Command - Exit/Multiple Command"
-bin/rshell << EOF
-echo "Hello world" && ls -a || mkdir test; git status
-exit
-EOF
-echo -e "\n"
-
-# Test 4: Tests exit at end of multiple command
-echo -e "Test 4: Exit Command - Exit/Multiple Command_End"
-bin/rshell << EOF
-echo "Hello world" || mkdir test; echo "Let's leave" && exit
-EOF
-echo -e "\n"
-
-# Test 5: Tests exit inside multiple commands
-echo -e "Test 5: Exit Command - Exit/Multiple Command_Mid"
-bin/rshell << EOF
-echo "Hello world" && exit; echo "So interesting" || echo "What a bummer"
-EOF
-echo -e "\n"
-
-# Test 6: Tests exit with commented command
-echo -e "Test 6: Exit Command - Exit/Comment Command"
-bin/rshell << EOF
-echo "Hello world"
-#exit
-echo "This is nice"
-exit
-EOF
-echo -e "\n"
-
-# Test 7: Tests exit with exit in a quotation
-echo -e "Test 7: Exit Command - Exit/\"exit\"_in-argument"
-bin/rshell << EOF
-echo "Hello world"; echo "Let us leave" && "Exit stage right"; git status
-echo "No, not that exit" || exit
-EOF
-echo -e "\n"
-
-# Cleans and exits
-make clean
+Script done on Fri 06 Nov 2015 09:13:02 PM PST
