ca414a315c0926f2b20c76822dcfc6ae668aac44
Rebecca Hom <rhom001@hammer.cs.ucr.edu>
2015-11-06 21:33:38 -0800

    cleaned up commented_command.sh

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -1,28 +1,28 @@
 Script started on Fri 06 Nov 2015 08:59:27 PM PST
-]0;rhom001@hammer:~/rshell[?1034h[rhom001@hammer rshell]$ make
+[rhom001@hammer rshell]$ make
 mkdir bin
 g++ -Wall -Werror -ansi -pedantic -o bin/rshell src/main.cpp src/rshell.cpp
-]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ bin/rshell
-rhom001@hammer$ echo "he  Hello world"; ls    
+[rhom001@hammer rshell]$ bin/rshell
+rhom001@hammer$ echo "Hello world"; ls
 "Hello world"
-rhom001@hammer$ ls -a  #-a
+rhom001@hammer$ ls #-a
 ls: cannot access : No such file or directory
 rhom001@hammer$ ls#-a
-bin  LICENSE  Makefile	README.md  rhom001  src  tests
-rhom001@hammer$ ls -a; eho  cho hello && mkdir         #mkdir test || echo world; git status
-.  ..  bin  .git  LICENSE  Makefile  README.md	rhom001  src  tests
+bin  LICENSE  Makefile	README.md  rhom001  src	 tests
+rhom001@hammer$ ls -a; echo hello && #mkdir test || echo world; git status
+.  ..  bin  .git  LICENSE  Makefile  README.md	rhom001	 src  tests
 hello
-rhom001@hammer$ E #echo "Failed comment       at commenting"
-rhom001@hammer$ echo Goodbt ye #&& ei xit
-Goodbye 
+rhom001@hammer$ #echo "Failed atmcommenting"
+rhom001@hammer$ echo Goodbye #&& exit
+Goodbye
 rhom001@hammer$ exit
 logout
-]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ bin/rshell
-rhom001@hammer$ ex cho | #hell  o || echo goodbye
+[rhom001@hammer rshell]$ bin/rshell
+rhom001@hammer$ echo #hello || echo goodbye
 
 rhom001@hammer$ exit
 logout
-]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ eix[K[Kxit
+[rhom001@hammer rshell]$ exit
 exit
 
 Script done on Fri 06 Nov 2015 09:02:25 PM PST
