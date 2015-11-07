2bfd6bb24917f5ed47286c121e68e7d608e6dcd5
Rebecca Hom <rhom001@hammer.cs.ucr.edu>
2015-11-06 20:32:16 -0800

    modified multi_command.sh

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -1,49 +1,123 @@
-#!/bin/sh
-# multi_command.sh
-# tests bin/rshell with multiple commands
-
-# Makes bin/rshell
-make
-echo "Multiple Commands Test"
-
-# Test 1: Tests the ","
-echo -e "Test 1: Multiple Commands - \";\"\n"
-bin/rshell << 'EOF'
-echo "Hello world!"; ls
-echo "This is another set"; cal; ls
-echo "This is the third set"; git status; ls; git log
+Script started on Fri 06 Nov 2015 08:18:04 PM PST
+]0;rhom001@hammer:~/rshell[?1034h[rhom001@hammer rshell]$ make
+mkdir bin
+g++ -Wall -Werror -ansi -pedantic -o bin/rshell src/main.cpp src/rshell.cpp
+]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ bin/rshell
+[rhom001@hammer] $ echo H "Hello world" && echo "And all those who inhabit it"
+"Hello world"
+"And all those who inhabit it"
+[rhom001@hammer] $ git status || ex  c date
+# On branch master
+# Your branch is ahead of 'origin/master' by 2 commits.
+#
+# Changed but not updated:
+#   (use "git add <file>..." to update what will be committed)
+#   (use "git checkout -- <file>..." to discard changes in working directory)
+#
+#	modified:   README.md
+#	modified:   tests/multi_command.sh
+#
+# Untracked files:
+#   (use "git add <file>..." to include in what will be committed)
+#
+#	bin/
+#	rhom001
+no changes added to commit (use "git add" and/or "git commit -a")
+[rhom001@hammer] $ git log g; echo    ls -a; e ge  src   sr  mkdir kit
+[?1h=commit 0a5306061d39aae269473eb07b4611dd4f281489[m
+Author: Rebecca Hom <rhom001@hammer.cs.ucr.edu>[m
+Date:   Fri Nov 6 20:17:31 2015 -0800[m
+[m
+    modified single command test script[m
+[m
+commit bb1ef1889706a1962517ea44268743f8a28f22c3[m
+Author: Rebecca Hom <rhom001@hammer.cs.ucr.edu>[m
+Date:   Fri Nov 6 19:43:58 2015 -0800[m
+[m
+    updated main to clear arrays at end of the program[m
+[m
+commit 22286fe7d3cb3c05e02a637d550c770a9ab17ce9[m
+Author: Rebecca Hom <rhom001@hammer.cs.ucr.edu>[m
+Date:   Thu Nov 5 20:49:55 2015 -0800[m
+[m
+    updated rshell[m
+[m
+commit ade6e097a2e7256128ad2f8e3d8ecccfdee85391[m
+Author: Rebecca Hom <rhom001@hammer.cs.ucr.edu>[m
+Date:   Thu Nov 5 20:47:34 2015 -0800[m
+[m
+    "tweaked || connector execution"[m
+:[K[K[?1l>.  ..  bin  .git  kit  LICENSE	Makefile  README.md  rhom001  src  tests
+mkdir: cannot create directory `kit': File exists
+[rhom001@hammer] $ mk  rmdir kit;  && mkdir things; vim things/test                   cd && echo "           src   things && mk  ls
+cd: No such file or directory
+[rhom001@hammer] $ ls
+bin  LICENSE  Makefile	README.md  rhom001  src  tests	things
+[rhom001@hammer] $ rm  dir things              cd things || rmdir things
+cd: No such file or directory
+[rhom001@hammer] $ ls -a
+.  ..  bin  .git  LICENSE  Makefile  README.md	rhom001  src  tests
+[rhom001@hammer] $ mkae    make    ma  mkdir kitt  && touch kit/test.t      magic.txt; mv mag   kit/magic.txt ..
+[rhom001@hammer] $ ls
+bin  kit  LICENSE  Makefile  README.md	rhom001  src  tests
+[rhom001@hammer] $ ls -a
+.  ..  bin  .git  kit  LICENSE	Makefile  README.md  rhom001  src  tests
+[rhom001@hammer] $ echo      mv kit/magic.txt t && echo           .. && echo Success!;   
+mv: cannot stat `kit/magic.txt': No such file or directory
+Success!
+[rhom001@hammer] $ echo "Well that i didn't work"
+"Well that didn't work"
+[rhom001@hammer] $ ls  ls  date &&    }}  || calc  l && calc    whom ami
+Fri Nov  6 20:25:11 PST 2015
+rhom001
+[rhom001@hammer] $ date && who; ec     || git lo      p vim magic.txt; exit
+Fri Nov  6 20:25:46 PST 2015
+jphan016 pts/0        2015-11-06 19:47 (97-93-103-140.static.mtpk.ca.charter.com)
+tprye001 pts/1        2015-11-06 18:07 (71-84-75-198.dhcp.rvsd.ca.charter.com)
+rhom001  pts/2        2015-11-06 19:39 (ucrwpa2-4-183-2.bulk.ucr.edu)
+jflor053 pts/3        2015-11-06 14:02 (169.235.73.116)
+jreim001 pts/4        2015-11-06 18:10 (cpe-76-95-0-184.socal.res.rr.com)
+mlauh001 pts/5        2015-11-06 20:07 (71-95-191-2.static.mtpk.ca.charter.com)
+kzhen002 pts/6        2015-11-06 18:25 (75-141-122-92.dhcp.mtpk.ca.charter.com)
+jchan107 pts/7        2015-11-06 18:17 (71-95-61-210.dhcp.rvsd.ca.charter.com)
+bpara001 pts/8        2015-11-06 16:40 (mobilenet1-4-161-120.bulk.ucr.edu)
+spica001 pts/9        2015-11-06 19:24 (kilo-20.cs.ucr.edu)
+ady002   pts/10       2015-11-06 15:46 (99-37-187-118.lightspeed.cornca.sbcglobal.net)
+kzhen002 pts/11       2015-11-06 20:13
+lniu001  pts/12       2015-11-06 17:20 (ucrwpa1-1-130-28.bulk.ucr.edu)
+ychen133 pts/13       2015-11-06 18:33 (lancetreo.dyn.ucr.edu)
+ady002   pts/14       2015-11-06 20:25
+emull005 pts/15       2015-11-06 17:26 (99-179-30-234.lightspeed.irvnca.sbcglobal.net)
+qluu001  pts/16       2015-11-06 19:00 (63.138.45.36)
+ddiep003 pts/17       2015-11-06 17:52 (kilo-27.cs.ucr.edu)
+dzhu006  pts/18       2015-11-06 18:35 (71-95-49-90.dhcp.rvsd.ca.charter.com)
+smagn001 pts/19       2015-11-06 12:51 (71-95-39-117.dhcp.rvsd.ca.charter.com)
+jalca011 pts/20       2015-11-06 17:54 (71-84-75-198.dhcp.rvsd.ca.charter.com)
+apuh001  pts/21       2015-11-06 17:52 (104.241.36.27)
+mcast052 pts/22       2015-11-06 18:38 (169.235.57.203)
+fyang016 pts/23       2015-11-06 20:24 (96-40-173-200.dhcp.mtpk.ca.charter.com)
+bpara001 pts/24       2015-11-06 18:42 (68-119-155-221.dhcp.ahvl.nc.charter.com)
+lzhan021 pts/25       2015-11-06 19:12 (99-109-89-176.lightspeed.irvnca.sbcglobal.net)
+mcast052 pts/26       2015-11-06 18:58 (169.235.57.203)
+lhern031 pts/27       2015-11-06 19:05 (75-141-121-119.dhcp.mtpk.ca.charter.com)
+tlars001 pts/28       2015-11-06 19:06 (138.229.143.23)
+mcast052 pts/29       2015-11-06 19:47 (169.235.57.203)
+cwong043 pts/30       2015-11-06 19:13 (71-95-45-183.dhcp.rvsd.ca.charter.com)
+bgutz001 pts/32       2015-11-06 19:50 (107-193-230-2.lightspeed.irvnca.sbcglobal.net)
+oli001   pts/34       2015-11-06 13:12 (104.241.36.27)
+mlauh001 pts/35       2015-11-06 20:16 (71-95-191-2.static.mtpk.ca.charter.com)
+cbent002 pts/36       2015-11-06 14:20 (63.138.45.36)
+bito001  pts/37       2015-11-06 20:07 (75-141-121-119.dhcp.mtpk.ca.charter.com)
+mcast052 pts/38       2015-11-06 20:11 (169.235.57.203)
+wrand001 pts/39       2015-11-06 20:16 (66-215-152-208.dhcp.rvsd.ca.charter.com)
+rhom001  pts/40       2015-11-06 20:18
+logout
+[rhom001@hammer] $ exit
+logout
+bin  kit  LICENSE  Makefile  README.md	rhom001  src  tests
+[rhom001@hammer] $ exit
+logout
+]0;rhom001@hammer:~/rshell[rhom001@hammer rshell]$ exit[K[K[K[Kexit
 exit
-EOF
-echo -e "\n"
 
-# Test 2: Tests the "&&"
-echo -e "Test 2: Multiple Commands - \"&&\"\n"
-bin/rshell << 'EOF'
-echo "Hello world" && echo "And all of its inhabitants"
-ls && echo "One echo" && echo "Two echoes"
-cd && echo "A middle connector" && git status && ls
-exit
-EOF
-echo -e "\n"
-
-# Test 3: Tests the "||"
-echo -e "Test 3: Multiple Commands - \"||\"\n"
-bin/rshell << 'EOF'
-echo "Hello world" || echo "It's today!"
-ls || echo "One echo" || "Two echoes"
-cd || echo "A middle connection" || git status
-exit
-EOF
-echo -e "\n"
-
-# Test 4: Tests a mix of connectors
-echo -e "Test 4: Multiple Commands - Multiple connectors"
-bin/rshell << 'EOF'
-echo "Hello world!" || echo "We have a problem"; ls
-git status && git log || echo "Error playing with git"
-ls; cd && "AND connector worked"
-exit
-EOF
-
-# Cleans up the area
-make clean
+Script done on Fri 06 Nov 2015 08:26:12 PM PST
