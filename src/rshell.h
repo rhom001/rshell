b1d72c2723a9a15158e9093b257b329a6415c2c6
Rebecca Hom <rhom001@hammer.cs.ucr.edu>
2015-11-20 13:11:49 -0800

    new rshell class functions (to be updated)

--- src/rshell.h
+++ src/rshell.h
@@ -11,11 +11,18 @@ class Rshell
     public:
         //  Class constructor
         Rshell();
+        //  Runs commands
+        void run(string& input);
         //  Parses command inputs
         void parseCommand(string& input, char line[][256], char* argv[][64]);
         void parseConnect(string&, unsigned, char line[][256], char, unsigned&, unsigned&);
+        //  Clears the array
         void clearArray(char line[][256]);
         void clearArrayP(char* argv[][64]);
+        //  Executes command inputs
         void executeCommand(char line[][256], char* argv[][64], bool);
+        //  Checks for a connector and compares
+        bool checkCon(char line[][256], unsigned);
+        bool connect(bool, bool, char line[][256], unsigned);
 };
 #endif  //  RSHELL_H
